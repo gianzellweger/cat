73f0e74ee8fed7529530a2e447f6f200ae26ba8c
Gian Zellweger <97334144+gianzellweger@users.noreply.github.com>
2022-05-22 23:49:00 +0200

    Improved the C implementation.
    This was done by using the syscall `fstat` and the `fread()` function.

--- cat.c
+++ cat.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include <sys/stat.h>
+
 
 int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("File not found\n");
+        return 1;
+    }
     FILE *file;
     file = fopen(argv[1], "rb");
     if (!file) {
         printf("File not found\n");
         return 1;
     }
-    char c;
-    while ((c = getc(file)) != EOF) printf("%c", c);
+    struct stat statbuf;
+    fstat(fileno(file), &statbuf);
+
+    char buffer[statbuf.st_size];
+    fread(buffer, statbuf.st_size, 1, file);
     fclose(file);
-    printf("\n");
-    return 0;
+    printf("%s\n", buffer);
 }
